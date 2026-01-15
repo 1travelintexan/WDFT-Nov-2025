edccdf41837a3c794c325cd13a4cbd7f6c77517d
Joshua <horheyinc@gmail.com>
2026-01-15 07:43:21 -0600

    auth

--- module-3/week-1/day-4/our-fullstack-app/server/routes/auth.routes.js
+++ module-3/week-1/day-4/our-fullstack-app/server/routes/auth.routes.js
@@ -1,17 +1,54 @@
 const router = require("express").Router();
 const UserModel = require("../models/User.model");
+
+const bcryptjs = require("bcryptjs");
+
 // a /signup route to create a new user with email, password and username
-router.post("/signup", (req, res) => {
-  UserModel.create(req.body)
-    .then((userCreated) => {
-      res.status(201).json({
-        message: "User created successfully!",
-        data: userCreated,
+router.post("/signup", async (req, res) => {
+  try {
+    const foundUser = await UserModel.findOne({ email: req.body.email });
+    if (foundUser) {
+      res.status(403).json({ errorMessage: "Email already taken" });
+    } else {
+      //generate the salt
+      const theSalt = bcryptjs.genSaltSync(12);
+      //generate the hashed password
+      const hashedPassword = bcryptjs.hashSync(req.body.password, theSalt);
+      console.log("the salt", theSalt);
+      console.log({ hashedPassword, password: req.body.password });
+      const createdUser = await UserModel.create({
+        ...req.body,
+        password: hashedPassword,
       });
-    })
-    .catch((err) => {
-      res.status(500).json({ errorMessage: err });
-    });
+      res.status(201).json(createdUser);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ errorMessage: err });
+  }
 });
 
+//login route
+router.post("/login", async (req, res) => {
+  try {
+    const foundUser = await UserModel.findOne({ email: req.body.email });
+    if (!foundUser) {
+      res.status(403).json({ errorMessage: "Email doesnt exist" });
+    } else {
+      const doesPasswordMatch = bcryptjs.compareSync(
+        req.body.password,
+        foundUser.password
+      );
+      console.log("Does password match?", doesPasswordMatch);
+      if (!doesPasswordMatch) {
+        res.status(403).json({ errorMessage: "Password incorrect" });
+      } else {
+        res.status(200).json({ message: "You are now logged in! Nice work" });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ errorMessage: err });
+  }
+});
 module.exports = router;
